767945364034ecae25d2b4b0da1c7070df68f42b
Czipperz <czipperz@gmail.com>
2016-03-24 14:15:34 -0500

    Reformat code

--- src/open_line.cc
+++ src/open_line.cc
@@ -6,57 +6,63 @@ namespace vick {
 namespace open_line {
 
 struct open_line_c : public change {
+private:
     const int num_lines;
     const move_t y;
-    open_line_c(size_t num_lines, move_t y) : num_lines(num_lines), y(y)
-    {
-        if (not num_lines) std::terminate();
+
+public:
+    open_line_c(int num_lines, move_t y)
+        : num_lines(num_lines)
+        , y(y) {
+        assert(num_lines != 0);
     }
     virtual bool is_overriding()
     {
         return true;
     }
-    virtual void undo(contents& contents)
-    {
+    virtual bool is_overriding() { return true; }
+    virtual void undo(contents& contents) {
         if (num_lines < 0) {
             contents.cont.erase(contents.cont.begin() + y,
-                                contents.cont.begin() + y - num_lines);
-            // contents.y = y // + num_lines
-                // ;
+                                contents.cont.begin() + y -
+                                    num_lines);
+            // contents.y = y /*+ num_lines*/;
         } else {
-            contents.cont.erase(contents.cont.begin() + y,
-                                contents.cont.begin() + y + num_lines);
+            contents.cont.erase(contents.cont.begin() + y + 1,
+                                contents.cont.begin() + y + 1 +
+                                    num_lines);
             // contents.y = y + num_lines;
         }
         contents.y = y;
         contents.x = 0;
     }
-    virtual void redo(contents& contents)
-    {
+    virtual void redo(contents& contents) {
         if (num_lines < 0) {
             for (int x = 0; x > num_lines; x--)
-                contents.cont.insert(contents.cont.begin() + y + x, "");
-            contents.y = y // + num_lines
-                ;
+                contents.cont.insert(contents.cont.begin() + y + x,
+                                     "");
+            contents.y = y /*+ num_lines*/;
         } else {
             for (int x = 1; x <= num_lines; x++)
-                contents.cont.insert(contents.cont.begin() + y + x, "");
+                contents.cont.insert(contents.cont.begin() + y + x,
+                                     "");
             contents.y = y + num_lines;
         }
         contents.x = 0;
     }
-    virtual std::shared_ptr<change> regenerate(const contents& contents) const
-    {
+    virtual std::shared_ptr<change>
+    regenerate(const contents& contents) const {
         return std::make_shared<open_line_c>(num_lines, contents.y);
     }
 };
 
 boost::optional<std::shared_ptr<change> >
-open_line_below(contents& contents, boost::optional<int> num)
-{
-    if (num and num.get() == 0) return boost::none;
+open_line_below(contents& contents, boost::optional<int> num) {
+    if (num and num.get() == 0)
+        return boost::none;
     std::shared_ptr<change> open_line =
-        std::make_shared<open_line_c>(num ? num.get() : 1, contents.y);
+        std::make_shared<open_line_c>(num ? num.get() : 1,
+                                      contents.y);
     open_line->redo(contents);
     auto insert = insert_mode::enter_insert_mode(contents);
     if (insert) {
@@ -69,10 +75,8 @@ open_line_below(contents& contents, boost::optional<int> num)
 }
 
 boost::optional<std::shared_ptr<change> >
-open_line_above(contents& contents, boost::optional<int> num)
-{
+open_line_above(contents& contents, boost::optional<int> num) {
     return open_line_below(contents, num ? -num.get() : -1);
 }
-
 }
 }
