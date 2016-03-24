b43ccfabaf8bf3240f585c41af236c322094fbdd
Czipperz <czipperz@gmail.com>
2016-03-24 14:13:15 -0500

    Reformat documentation

--- src/open_line.hh
+++ src/open_line.hh
@@ -8,21 +8,23 @@ namespace open_line {
 
 /*!
  * \file open_line.hh
- * \brief Describes the open_line_below() and open_line_above() functions to insert newlines.
+ * \brief Describes the open_line_below() and open_line_above()
+ * functions to insert newlines.
  */
 
 /*!
- * \brief Makes a given number of newlines after the current line and enters insert mode
+ * \brief Makes a given number of newlines after the current line and
+ * enters insert mode
  */
 boost::optional<std::shared_ptr<change> >
 open_line_below(contents&, boost::optional<int> = 1);
 
 /*!
- * \brief Makes a given number of newlines before the current line and enters insert mode
+ * \brief Makes a given number of newlines before the current line and
+ * enters insert mode
  */
 boost::optional<std::shared_ptr<change> >
 open_line_above(contents&, boost::optional<int> = 1);
-
 }
 }
 
